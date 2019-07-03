278fb7de7e77aaec9bec5bd18dba4070dd3f5b85
Ricardo Rodriguez <rr092293@gmail.com>
2019-07-03 03:56:29 -0700

    added views and templates

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 
 from . import views
-from .views import EventList
+from .views import EventList, EventDetail
 
+
+app_name = 'polls'
 urlpatterns = [
     # path('', views.index, name='index'),
     path("events/", EventList.as_view(), name="event_list"),
+    path('<int:event_id>/', EventDetail.as_view(), name='detail'),
 ]
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,8 +1,8 @@
-# from django.shortcuts import render
-# from django.http import HttpResponse
+
+
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from django.shortcuts import get_object_or_404
+from django.shortcuts import get_object_or_404, render
 
 from .models import Event, Event_Item
 from .serializers import EventSerializer
@@ -11,7 +11,14 @@ class EventList(APIView):
     def get(self, request):
         events = Event.objects.all()[:20]
         data = EventSerializer(events, many=True).data
-        return Response(data)
+        return render(request, 'events/index.html', {'latest_event_list': data})
+
+
+class EventDetail(APIView):
+    def get(self, request, event_id):
+        event = Event.objects.get(id=event_id)
+        data = EventSerializer(event, many=False).data
+        return render(request, 'events/detail.html', {'event': data})
 
 # def index(request):
 #     return HttpResponse("Hello, world. You're at the polls index.")
