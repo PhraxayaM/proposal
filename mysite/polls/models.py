7f16e8aa135062fbbb26bba99dc4ff3de090d25c
MattHew Phraxayavong <mphraxa@gmail.com>
2019-07-03 01:56:00 -0700

    admin and models setup complete

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -4,12 +4,12 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
-class Question(models.Model):
-    question_text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField('date published')
+class Event(models.Model):
+    event_name = models.CharField(max_length=200)
+    event_date = models.DateField('event date')
 
 
-class Choice(models.Model):
-    question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    choice_text = models.CharField(max_length=200)
-    votes = models.IntegerField(default=0)
+class Event_Item(models.Model):
+    event = models.ForeignKey(Event, on_delete=models.CASCADE)
+    item_text = models.CharField(max_length=200)
+    item_time = models.TimeField('Start time')
